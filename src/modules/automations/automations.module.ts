33558a459072a2ddefb1036b0897a7136c946bc4
Kleber A dos Santos <kleber.dossantos001@mymdc.net>
2026-07-09 15:00:02 -0400

    feat(automations): registrar DelayHandler no registry e módulo

--- src/modules/automations/automations.module.ts
+++ src/modules/automations/automations.module.ts
@@ -17,6 +17,7 @@ import { AddToPipelineHandler } from './actions/handlers/add-to-pipeline.handler
 import { MovePipelineStageHandler } from './actions/handlers/move-pipeline-stage.handler';
 import { AssignUserHandler } from './actions/handlers/assign-user.handler';
 import { SendMessageHandler } from './actions/handlers/send-message.handler';
+import { DelayHandler } from './actions/handlers/delay.handler';
 import { AutomationsService } from './automations.service';
 import { AutomationsController } from './automations.controller';
 import { AutomationsRunsController } from './automations-runs.controller';
@@ -54,6 +55,7 @@ import { AUTOMATION_QUEUE } from './automations.constants';
     MovePipelineStageHandler,
     AssignUserHandler,
     SendMessageHandler,
+    DelayHandler,
     ActionRegistryService,
   ],
   exports: [OutboxService, KillSwitchService],
